13865af36a58de5c26ce99d36c3c0a2ed3860480
jakegaughan <jakegaughan5@gmail.com>
2020-07-17 23:47:47 -0500

    biggy

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,50 +1,136 @@
-const box1 = document.querySelector('#box1')
-const box1X = document.querySelector('#box1X')
-const box2X = document.querySelector('#box2X')
-const box3X = document.querySelector('#box3X')
-const box4X = document.querySelector('#box4X')
-const box5X = document.querySelector('#box5X')
-const box6X = document.querySelector('#box6X')
-const box7X = document.querySelector('#box7X')
-const box8X = document.querySelector('#box8X')
-const box9X = document.querySelector('#box9X')
-const box1O = document.querySelector('#box1O')
-const box2O = document.querySelector('#box2O')
-const box3O = document.querySelector('#box3O')
-const box4O = document.querySelector('#box4O')
-const box5O = document.querySelector('#box5O')
-const box6O = document.querySelector('#box6O')
-const box7O = document.querySelector('#box7O')
-const box8O = document.querySelector('#box8O')
-const box9O = document.querySelector('#box9O')
+const difficulties = document.querySelectorAll('.difficulty')
+const box = document.querySelectorAll('.boxes')
+const o = document.querySelectorAll('.o')
+const x = document.querySelectorAll('.x')
+const x1 = document.querySelectorAll('.x1')
+const x2 = document.querySelectorAll('.x2')
+var endGameBanner = document.querySelector('#endGameBanner')
+var isSet = []
+var choice
+var setDifficulty = 'easy'
+var difficulty = difficulties[0].innerText
 
-const Xs = document.querySelectorAll('x')
+currentDifficulty()
 
-Xs.forEach(x =>{
-    x.style.display = "none"
-})
+for(let i = 0; i < 9; i++){
+    isSet[i] = false
+}
+var gameOver = isSet.every((item) => item)
 
-function markBox1(){
-    if(box1X.style.display === "none" && box1O.style.display === "none"){
-        box1X.style.display = "block"
-    }
-    else{
-        console.log(box1O.style.display)
-        console.log(box1X.style.display)
-        box1X.style.display = "none"
-        box1O.style.display = "none"
-        console.log(box1O.style.display)
+for(let i = 0; i < 9; i++){
+    box[i].addEventListener('click', ()=>{
+        if(gameOver){
+            getWinner()
+        }else{
+            if(!isSet[i]){
+                isSet[i] = true
+                x1[i].style.width = "9.5rem",
+                x2[i].style.width = "9.5rem"
+                
+                checkWinner()
+                switch(setDifficulty){
+                    case 'easy':
+                        compMoveEasy()
+                        break
+                    case 'medium':
+                        compMoveMedium()
+                        break
+                    default:
+                        compMoveImpossible()
+                }
+                
+            }
+            else{
+                box[i].style.backgroundColor = 'red'
+                setTimeout(()=> box[i].style.backgroundColor = '' , 500)
+            }
+        }
+            
+    })
+}
+
+function currentDifficulty(){
+    switch(difficulty){
+        case 'EASY':
+            difficulties[0].style.border = ".3rem solid #000000"
+            break
+        case 'MEDIUM':
+            difficulties[1].style.border = ".3rem solid #000000"
+            break
+        default:
+            difficulties[2].style.border = ".3rem solid #000000"
     }
 }
 
-box1.addEventListener('click', markBox1)
-// box2.addEventListener('click', markBox1)
-// box3.addEventListener('click', markBox1)
-// box4.addEventListener('click', markBox1)
-// box5.addEventListener('click', markBox1)
-// box6.addEventListener('click', markBox1)
-// box7.addEventListener('click', markBox1)
-// box8.addEventListener('click', markBox1)
-// box9.addEventListener('click', markBox1)
+function setToEasy(){
 
-box1
+}
+
+function setToMedium(){
+
+}
+
+function setToImpossible(){
+
+}
+
+function compMoveEasy(){
+        if(!gameOver){
+            choice = Math.floor(Math.random() * 9)
+            while (isSet[choice]) {
+                choice = Math.floor(Math.random() * 9)
+            }
+            isSet[choice] = true
+            sleep(500).then(() =>{
+                o[choice].style.transform = "translateY(0rem)",
+                o[choice].style.width = "8rem",
+                o[choice].style.height = "8rem",
+                o[choice].style.border = "0.5rem solid #2c7a7b"
+            })
+            checkWinner()
+        }
+}
+
+function checkWinner(){
+    gameOver = isSet.every((item) => item)
+    if(gameOver){
+        getWinner()
+    }
+}
+
+function getWinner(){
+    endGameBanner.style.display = "flex"
+    endGameBanner.style.opacity = "1"
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+difficulties.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        difficulty = item.innerText
+        switch(difficulty){
+            case 'EASY':
+                setDifficulty = 'easy'
+                difficulties[0].style.border = ".3rem solid #000000"
+                difficulties[1].style.border = "none"
+                difficulties[2].style.border = "none"
+                setToEasy()
+                break
+            case 'MEDIUM':
+                setDifficulty = 'medium'
+                difficulties[1].style.border = ".3rem solid #000000"
+                difficulties[0].style.border = "none"
+                difficulties[2].style.border = "none"
+                setToMedium()
+                break
+            default:
+                setDifficulty = 'impossible'
+                difficulties[2].style.border = ".3rem solid #000000"
+                difficulties[0].style.border = "none"
+                difficulties[1].style.border = "none"
+                setToImpossible()
+        }
+    })
+})
